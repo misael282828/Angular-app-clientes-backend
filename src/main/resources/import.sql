a97e9bfea01c9cf055f50801b8456df466ece1f1
misael282828 <misael282828@gmail.com>
2023-06-30 23:59:09 -0400

    probando

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
 INSERT INTO cliente (nombre, apellido, email ,create_at) VALUES('Pablo', 'Perez','Pablo@test.com', '2023-01-01');
 INSERT INTO cliente (nombre, apellido, email ,create_at) VALUES('Maria', 'Perez','Maria@test.com', '2023-02-01');
-INSERT INTO cliente (nombre, apellido, email ,create_at) VALUES('Juan', 'Perez','Juan@test.com', '2020-02-01');
+INSERT INTO cliente (nombre, apellido, email ,create_at) VALUES('Juan', 'Perez','Juan@test.com', '2023-02-01');
 INSERT INTO cliente (nombre, apellido, email ,create_at) VALUES('Alberto', 'Perez','Alberto@test.com', '2023-05-01');
 INSERT INTO cliente (nombre, apellido, email ,create_at) VALUES('Misael', 'Guzman','test@test.com', '2023-01-01');
 
